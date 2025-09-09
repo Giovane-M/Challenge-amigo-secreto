8c8b941e80d592351a4a0709833a1d1f9147833f
Giovane Marcilio <giovanemarcilio@outlook.com>
2025-09-08 22:53:01 -0300

    Implementação da exibição da lista de amigos

--- challenge-amigo-secreto_pt-main/app.js
+++ challenge-amigo-secreto_pt-main/app.js
@@ -1,4 +1,3 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
 let listaAmigos = [];
 
 function exibirTextoNaTela(tag, texto) {
@@ -8,16 +7,29 @@ function exibirTextoNaTela(tag, texto) {
 exibirTextoNaTela('h1','Amigo Secreto');
 exibirTextoNaTela('h2','Digite o nome dos seus amigos');
 
+function atualizarListaHTML() {
+    let lista = document.getElementById('lista-amigos');
+    lista.innerHTML = '';
+
+    for(let i = 0; i < listaAmigos.length; i++) {
+        let item = document.createElement('li');
+        item.textContent = listaAmigos[i];
+        lista.appendChild(item);
+    }
+}
+
 function adicionarAmigo() {
     let add = document.querySelector('input').value;
     
     if (add === "") {
-        alert('Digite um nome válido');
+        alert('Por favor, insira um nome');
         return;
     }
     listaAmigos.push(add);
     document.querySelector('input').value = "";
     exibirTextoNaTela('h2', 'Lista: ' + listaAmigos.join(", "));
+    atualizarListaHTML();
+
 }
 
 function sortearAmigo() {
